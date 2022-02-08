b8fe5bb57418ae67a1c3766f32b049d5cda1d43c
Abanisenioluwa Kolawole Orojo <b.korojo@gmail.com>
2022-02-08 13:57:52 +0100

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,13 +2,14 @@
 
 import subprocess
 import optparse
+import re
 
 
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-    (options, arguments) =  parser.parse_args()
+    (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info")
     elif not options.new_mac:
@@ -23,5 +24,25 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC Address ")
+
+
+def changed_mac_address(mac, new_mac):
+    if mac == new_mac:
+        print("MAC changed ")
+    else:
+        print("MAC Address not changed")
+
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+changed_mac_address(current_mac, options.new_mac)
