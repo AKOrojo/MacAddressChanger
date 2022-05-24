a475396eaf23709de9ac8ffef632b2b51ccea29b
Abanisenioluwa Kolawole Orojo <b.korojo@gmail.com>
2022-05-24 13:32:52 +0100

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -7,42 +7,49 @@ import re
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more info")
+        parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
-        parser.error("[-] Please specify a new MAC Address, use --help for more info")
+        parser.error("[-] Please specify an new mac, use --help for more info.")
     return options
 
 
 def change_mac(interface, new_mac):
-    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
 
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    ifconfig_result_encoded = subprocess.check_output(["ifconfig", interface])
+    ifconfig_result = ifconfig_result_encoded.decode("utf-8")
     mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
     if mac_address_search_result:
         return mac_address_search_result.group(0)
     else:
-        print("[-] Could not read MAC Address ")
+        print("[-] Could not read MAC address.")
 
 
 def changed_mac_address(mac, new_mac):
     if mac == new_mac:
-        print("MAC changed ")
+        print("[+] MAC changed to " + new_mac)
     else:
-        print("MAC Address not changed")
+        print("[-] MAC Address not changed")
 
 
 options = get_arguments()
+
 current_mac = get_current_mac(options.interface)
 print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.new_mac)
 current_mac = get_current_mac(options.interface)
 changed_mac_address(current_mac, options.new_mac)
+
+
+
+
